e325c90389d0d36aa67b2116c0a4262040b3628a
triin <triinlemba@gmail.com>
2026-09-17 15:17:36 +0300

    Update reset database. Empty out 2_create.sql, 3_import.sql

--- docs/database/1_reset_database.sql
+++ docs/database/1_reset_database.sql
@@ -1,7 +1,7 @@
 -- Kustutab minu_projekt schema (mis põhimõtteliselt kustutab kõik tabelid)
-DROP SCHEMA IF EXISTS minu_projekt CASCADE;
+DROP SCHEMA IF EXISTS testi_platvorm CASCADE;
 -- Loob uue minu_projekt schema vajalikud õigused
-CREATE SCHEMA minu_projekt
+CREATE SCHEMA testi_platvorm
 -- taastab vajalikud andmebaasi õigused
-    GRANT ALL ON SCHEMA minu_projekt TO postgres;
-GRANT ALL ON SCHEMA minu_projekt TO PUBLIC;
+    GRANT ALL ON SCHEMA testi_platvorm TO postgres;
+GRANT ALL ON SCHEMA testi_platvorm TO PUBLIC;
