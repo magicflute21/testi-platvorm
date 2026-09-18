65708ce16a07cec37e3b46193884b6c674b058ec
triin <triinlemba@gmail.com>
2026-09-18 16:08:37 +0300

    LoginView basic scaffolding.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import TestView from "@/views/TestView.vue";
+import LoginView from '@/views/LoginView.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/test',
       name: 'testRoute',
       component: TestView,
+    },
+    {
+      path: '/login',
+      name: 'loginRoute',
+      component: LoginView,
     },
   ],
 })
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'LoginView',
+}
+</script>
+
+<template>
+  <div class="container text-start col-3 mt-5">
+    <div class="row">
+      <h1>Logi sisse</h1>
+      <p>Sisesta oma konto email ja salasõna.</p>
+      <form>
+        <div class="mb-3">
+          <input
+            type="email"
+            class="form-control"
+            aria-describedby="emailHelp"
+            placeholder="Email"
+          />
+          <div id="emailHelp" class="form-text">Me ei jaga su emaili teistega.</div>
+        </div>
+        <div class="mb-3">
+          <input type="password" class="form-control" placeholder="Salasõna" />
+        </div>
+        <button type="submit" class="btn btn-primary">Logi sisse</button>
+      </form>
+    </div>
+  </div>
+</template>
