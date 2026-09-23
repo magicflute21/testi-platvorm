887c6188b933b413360e86966f78059358d549df
Anngrett <anngrett.sahtel@gmail.com>
2026-09-23 09:14:33 +0300

    TP-8 sisse logimise front ja back, WIP2

--- backend/src/main/java/ee/testiplatvorm/persistence/UserRepository.java
+++ backend/src/main/java/ee/testiplatvorm/persistence/UserRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query("select u from User u where u.email = :email and u.passwordHash = :passwordHash")
-    Optional<User> findUserBy(String email, String passwordHash);
-
-    if(
+    @Query("select u from User u where u.email = :email and u.passwordHash = :passwordHash and u.status = :status")
+    Optional<User> findUserBy(String email, String passwordHash, String status);
 
 }
