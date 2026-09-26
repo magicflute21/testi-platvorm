22f1393b4184fb8109410da75876688eb5942681
puksand <itteenused6@gmail.com>
2026-09-26 16:40:08 +0300

    WIP- tehtud user tabeli ja profile tabeli join, meetod findAllUserResponse

--- backend/src/main/java/ee/testiplatvorm/persistence/user/UserRepository.java
+++ backend/src/main/java/ee/testiplatvorm/persistence/user/UserRepository.java
@@ -12,10 +12,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("select u from User u where u.email = :email and u.passwordHash = :passwordHash and u.status = :status")
     Optional<User> findUserBy(String email, String passwordHash, String status);
 
-//    @Query("""
-//     select new  ee.testiplatvorm.controller.user.dto.UserResponse (i.id, e.email, s.status)
-//                from User u
-//                 left join p.Profile p """)
-//    List<UserResponse> findAllUserResponses();
+    @Query("""
+     select new  ee.testiplatvorm.controller.user.dto.UserResponse (u.id, p.firstName, p.lastName, u.email, u.role.name, u.status)
+                from User u left join Profile p on u.id = p.user.id """)
+    List<UserResponse> findAllUserResponses();
 
 }
--- backend/src/main/java/ee/testiplatvorm/service/UserService.java
+++ backend/src/main/java/ee/testiplatvorm/service/UserService.java
@@ -1,12 +1,21 @@
 package ee.testiplatvorm.service;
 
 
+import ee.testiplatvorm.controller.user.dto.UserResponse;
+import ee.testiplatvorm.persistence.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
     public void findAllUsers() {
+        List<UserResponse> allUserResponses = userRepository.findAllUserResponses();
+
+
     }
 }
